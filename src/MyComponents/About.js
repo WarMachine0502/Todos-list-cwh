6d2f09750bf95423a535f856c63bf16c62f10693
WarMachine0502 <sahasradthakur@gmail.com>
2022-10-15 22:31:31 +0530

    Done.

---
+++ src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        This is an about component
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi officia sint possimus beatae ab recusandae, veniam, libero debitis odit asperiores tenetur eveniet cupiditate necessitatibus distinctio expedita deleniti corrupti qui illo, tempore itaque laudantium. Modi.
+        </p>
+    </div>
+  )
+}
